3c79cf147848624b0dfffe4f2ac50d8f91b4d0ed
darkogligorijevic <darkogligorijeviccc@gmail.com>
2023-11-18 05:21:35 +0100

    Fixing issues/Adding more features

--- main.py
+++ main.py
@@ -35,35 +35,48 @@ morse_code_dict = {
 # Function that transform a text to a morse code
 def text_to_morse():
 
-    text = entry.get()
+    text_result = ''
 
-    for letter in text.upper():
-        if letter == ' ':
-            time.sleep(1)
-        else:
-            for char in morse_code_dict[letter]:
-                if char == '.':
+    text_entry = entry.get().upper()
+    split_space = text_entry.split(' ')
+
+    for word in split_space:
+        for letter in word:
+            for symbol in morse_code_dict[letter]:
+                if symbol == '.':
                     winsound.Beep(1000, 200)
-                elif char == '-':
+                elif symbol == '-':
                     winsound.Beep(1000, 400)
-        time.sleep(0.2)
+            text_result += ''.join(morse_code_dict[letter]) + ' '
+            time.sleep(0.2)
+        time.sleep(0.5)
+        text_result += '/'
+
+    label_result.config(text=text_result[:-1])
 
 # Function that transform a morse code to a text
 def morse_to_text():
 
-    morse_result = ""
+    morse_result = ''
+    current_morse = ''
     morse_entry = entry.get()
-    morse_list = morse_entry.split(' ')
 
-    for each_morse in morse_list:
-        morse_key = [k for k, v in morse_code_dict.items() if v == each_morse]
-        morse_result += ''.join(morse_key)
-        label_result.config(text=morse_result)
+    split_slash = morse_entry.split('/')
+
+    for word in split_slash:
+        split_space = word.split(' ')
+        current_morse = ''
+        for symbol in split_space:
+            morse_key = [k for k, v in morse_code_dict.items() if v == symbol]
+            current_morse += ''.join(morse_key)
+        morse_result += current_morse + ' '
+    
+    label_result.config(text=morse_result)
         
 
-
+# Declaring tkinter 
 root = tk.Tk()
-root.title("Morse Code Converter")
+root.title('Morse Code Converter')
 
 # Window size
 root.minsize(400, 200)
@@ -71,26 +84,26 @@ root.maxsize(800, 400)
 
 # Frame for a better style
 frame = tk.Frame(root, pady=10)
-frame.pack(expand=True, fill="both")
+frame.pack(expand=True, fill='both')
 
 # Label for entry field
-label_text = tk.Label(frame, text="Enter text: ", font=("Helvetica", 12))
+label_text = tk.Label(frame, text='Enter text: ', font=('Helvetica', 12))
 label_text.pack(padx=8)
 
-# Entry widget
-entry = tk.Entry(frame, font=("Helvetica", 14))
-entry.pack(pady=10, padx=10, fill="both")
+# Entry widget (input field)
+entry = tk.Entry(frame, font=('Helvetica', 14))
+entry.pack(pady=10, padx=10, fill='both')
 
 # Button for text to morse
-button1 = tk.Button(frame, text="Convert to Morse Code", command=text_to_morse, font=("Helvetica", 12))
+button1 = tk.Button(frame, text='Convert to Morse Code', command=text_to_morse, font=('Helvetica', 12))
 button1.pack(pady=10)
 
 # Button for morse to text
-button2 = tk.Button(frame, text="Convert to Text", command=morse_to_text, font=("Helvetica", 12))
+button2 = tk.Button(frame, text='Convert to Text', command=morse_to_text, font=('Helvetica', 12))
 button2.pack(pady=10)
 
-# Label that displays result 
-label_result = tk.Label(frame, text="", font=("Helvetica", 24))
+# Label that displays results 
+label_result = tk.Label(frame, text='', font=('Helvetica', 24))
 label_result.pack(padx=20)
 
 # Main loop
